856a2f777f3711284d4be0a5578151d63e86a922
kdm111 <pjsu94@naver.com>
2024-06-25 16:43:31 +0900

    init : gradle 설정 오류로 인한 재설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 plugins {
     java
+    application
+}
+application {
+    mainClass = "site.pixeldetective.server.Main"
 }
 
-group = "site.pixeldetective"
+group = "site.pixeldetective.server"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,10 +14,9 @@ repositories {
 }
 
 dependencies {
-    // JSON-Java 라이브러리 추가
     implementation("org.json:json:20210307")
-    // MySQL Connector 라이브러리 추가
     implementation("mysql:mysql-connector-java:8.0.29")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
@@ -21,3 +24,4 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
