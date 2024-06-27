dfeaeedb64358b0848d6c68d1db8fa9caf47bca7
whdudgms <108641428+whdudgms@users.noreply.github.com>
2024-06-27 23:40:29 +0900

    JWT 발급 기능 생성 같이 있는 GET의 경우 추후에 지움

--- src/main/java/site/pixeldetective/server/router/LoginHandler.java
+++ src/main/java/site/pixeldetective/server/router/LoginHandler.java
@@ -4,22 +4,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import site.pixeldetective.server.dao.ChatDAO;
 import site.pixeldetective.server.dao.LoginDAO;
 import site.pixeldetective.server.dao.UserDAO;
+import site.pixeldetective.server.dto.ChatDTO;
 import site.pixeldetective.server.dto.LoginDTO;
 import site.pixeldetective.server.dto.UserDTO;
 import site.pixeldetective.server.jwt.JwtSender;
 
-public class LoginHandler implements HttpHandler{
+import static site.pixeldetective.server.router.IndexRouter.queryToMap;
 
+public class LoginHandler implements HttpHandler{
+	JwtSender js = new JwtSender();
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
 		String response = "";
@@ -44,18 +52,37 @@ public class LoginHandler implements HttpHandler{
 				JSONObject jsonResponse = new JSONObject();
 				jsonResponse.put("message", "실패");
 				response = jsonResponse.toString();
+				exchange.getResponseHeaders().set("Authorization", "false fail " );
 
 			}else {
 				statusCode = 201;
 				JSONObject jsonResponse = new JSONObject();
 				jsonResponse.put("message", "성공");
-				response = jsonResponse.toString();
 
-				JwtSender js = new JwtSender();
+
 				// JWT를 받아옴 서버 메서드 호출
+				// 헤더에 저장해서 보내봤는데도 어떻게 안 되서 그냥 이렇게 응답메세지에 보냄
 				String jwt = js.createJWT(rowAffected.getuId(), rowAffected.getuNum(), rowAffected.getuName(), rowAffected.getuId());
-				exchange.getResponseHeaders().set("Authorization", "Bearer " + jwt);
+
+				jsonResponse.put("jwt", jwt);
+				response = jsonResponse.toString();
 			}
+		}else if(exchange.getRequestMethod().equalsIgnoreCase("GET")){
+			System.out.println(2131312);
+
+			URI requestURI = exchange.getRequestURI();
+			Map<String, String> queryParams = queryToMap(requestURI.getQuery());
+			String jwt = queryParams.get("jwt");
+
+			// JSON으로 변환
+			boolean result = js.verifyJWT(jwt);
+
+			System.out.println(result);
+			System.out.println(result);
+			JSONObject jsonResponse = new JSONObject();
+			jsonResponse.put("result", result);
+
+			response = jsonResponse.toString();
 		}
 		exchange.getResponseHeaders().set("Content-Type", "application/json");
         exchange.sendResponseHeaders(statusCode, response.getBytes().length);
